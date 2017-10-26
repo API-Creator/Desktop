c12532acd080ecb8efd376f34f04f27ddfeeec1d
Kim0Jae <youngjae1047@naver.com>
2017-10-27 01:13:46 +0900

    font 추가 및 적용 테스트

---
+++ src/main/java/BasePane.java
@@ -0,0 +1,11 @@
+import javafx.application.Application;
+import javafx.scene.text.Font;
+import javafx.stage.Stage;
+
+public class BasePane extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+    }
+}
+
--- src/main/java/login/Main.java
+++ src/main/java/login/Main.java
@@ -2,6 +2,7 @@ package login;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -10,6 +11,7 @@ import javafx.scene.Scene;
 public class Main extends Application {
     @Override
     public void start(Stage primaryStage) {
+        Font.loadFont(getClass().getResourceAsStream("resource/fonts/spoqa/SpoqaHanSans-Bold.ttf"), 14);
         try {
             Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
             primaryStage.setScene(new Scene(root, 900, 550));
